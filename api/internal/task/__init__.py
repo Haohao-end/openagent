ecd009e162e8c8469c55878fa7bdf948e8494851
Haohao-end <2227625024@qq.com>
2026-04-06 16:55:13 +0800

    chore: trim redundant package headers

--- api/internal/task/__init__.py
+++ api/internal/task/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2024/3/29 10:44
-@Author  : thezehui@gmail.com
-@File    : __init__.py.py
-"""
-
 from internal.task import app_task, dataset_task, document_task, email_task
